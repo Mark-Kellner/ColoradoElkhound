dfca19eda4c3949b3ffc8e8df4c016d7a97f677f
Mark-Kellner <mark.kellner@gmail.com>
2025-08-29 11:25:26 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Language" content="en-us">
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<meta name="GENERATOR" content="Microsoft FrontPage 4.0">
+<meta name="ProgId" content="FrontPage.Editor.Document">
+<title>ColoradoElkhound.Com - Home Page</title>
+</head>
+
+<body background="bg.gif" bgproperties="fixed">
+
+<div align="center">
+  <center>
+  <table border="0" width="760" cellspacing="0" cellpadding="0">
+			<?php include("/home/colorado/public_html/coloradoelkhound/public/v2/php/logo.php");  ?>
+    <tr>
+      <td width="152" bgcolor="#0080FF" valign="top">
+        </center>
+      <table border="0" width="100%" cellspacing="0" cellpadding="0">
+			<?php include("/home/colorado/public_html/coloradoelkhound/public/v2/php/int.php");  ?>
+			<?php include("/home/colorado/public_html/coloradoelkhound/public/v2/php/in.php");  ?>
+			<?php include("/home/colorado/public_html/coloradoelkhound/public/v2/php/inb.php");  ?>
+      </table>
+      <p align="center"><font color="#00FFFF" face="Arial"><a href="mailto:webmaster@coloradoelkhound.com">Webmaster</a></font>
+      <p align="center">&nbsp;
+      </td>
+      <td width="615" bgcolor="#0080FF" valign="top">
+        <p align="center">&nbsp;
+        <table border="0" width="100%" cellspacing="0" cellpadding="0">
+          	<!--<?php include("/home/colorado/public_html/coloradoelkhound/public/v2/php/mft.php");  ?>-->
+			<?php include("/home/colorado/public_html/coloradoelkhound/public/v2/php/mf.php");  ?>
+          	<!--<?php include("/home/colorado/public_html/coloradoelkhound/public/v2/php/mfb.php");  ?>-->
+        </table>
+    </td>
+    </tr>
+  </table>
+</div>
+
+</body>
+
+</html>
